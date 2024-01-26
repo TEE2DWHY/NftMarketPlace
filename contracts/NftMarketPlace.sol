5bb7380c7c6eb3b4d684ee7c9ab00142de9aca00
TEE2DWHY <isaacolorunfemi330@gmail.com>
2024-01-26 23:57:15 +0100

    added update item function and withdraw function

--- contracts/NftMarketPlace.sol
+++ contracts/NftMarketPlace.sol
@@ -11,6 +11,8 @@ error NftMarketPlace__PriceNotMet(
     uint256 tokenId,
     uint256 price
 );
+error NftMarketPlace_NoProceeds();
+error NftMarketPlace__TransferFailed();
 
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
@@ -30,7 +32,17 @@ contract NftMarketPlace is ReentrancyGuard {
         uint256 price
     );
 
-    event CanceledItem(address indexed nftAddress, uint256 indexed tokenId);
+    event CanceledItem(
+        address spender,
+        address indexed nftAddress,
+        uint256 indexed tokenId
+    );
+
+    event UpdatedItem(
+        address indexed nftAddress,
+        uint256 tokenId,
+        uint256 newPrice
+    );
 
     struct Listing {
         uint256 price;
@@ -69,6 +81,13 @@ contract NftMarketPlace is ReentrancyGuard {
         emit ItemListed(msg.sender, nftAddress, tokenId, price);
     }
 
+    /*
+     *notice Method for buying your NFT on the marketplace
+     *@param nftAddress: address of the NFT
+     *@param tokenId: Token Id of the NFT
+     @dev, we had to attach a user to his proceeds (sales) and move the risk to the user,
+      by having them withdraw the tokens by themselves and not us transferring it to them.
+     */
     // buy item
     function buyItem(
         address nftAddress,
@@ -85,26 +104,62 @@ contract NftMarketPlace is ReentrancyGuard {
         s_proceeds[listedItem.seller] +=
             s_proceeds[listedItem.seller] +
             msg.value;
+        delete (s_listings[nftAddress][tokenId]);
         ERC721URIStorage nft = ERC721URIStorage(nftAddress);
         nft.safeTransferFrom(listedItem.seller, msg.sender, tokenId);
         emit ItemBought(msg.sender, nftAddress, tokenId, listedItem.price);
-        delete (s_listings[nftAddress][tokenId]);
     }
 
+    /*
+     *notice Method to cancel a listed item
+     @spender: Address of the  NFT Owner
+     @toekenId: Token Id of the NFT
+     */
     // cancel item lisitng
     function cancelItem(
         address nftAddress,
         address spender,
         uint256 tokenId
-    ) external {
+    )
+        external
+        isOwer(nftAddress, tokenId, msg.sender)
+        isListed(nftAddress, tokenId)
+    {
         spender = address(this);
-        ERC721URIStorage nft = ERC721URIStorage(nftAddress);
-        // check if NFT is not approved
-        if (nft.getApproved(tokenId) != spender) {
-            revert NftMarketPlace__NftNotApprovedByMarketPlace();
+        // ERC721URIStorage nft = ERC721URIStorage(nftAddress);
+        // nft.approve(address(0), tokenId);
+        delete (s_listings[nftAddress][tokenId]);
+        emit CanceledItem(msg.sender, nftAddress, tokenId);
+    }
+
+    //  update the item
+    function updateItem(
+        address nftAddress,
+        uint256 tokenId,
+        uint256 newPrice
+    )
+        external
+        payable
+        isOwer(nftAddress, tokenId, msg.sender)
+        isListed(nftAddress, tokenId)
+    {
+        // Listing memory updatedaList = s_listings[nftAddress][tokenId];
+        // updatedaList.price = newPrice; // this is more gas expensive
+        s_listings[nftAddress][tokenId].price = newPrice; // this is more gas efficient
+        emit UpdatedItem(nftAddress, tokenId, newPrice);
+    }
+
+    // withdraw proceeds
+    function withdraw() external {
+        uint256 proceeds = s_proceeds[msg.sender];
+        if (proceeds <= 0) {
+            revert NftMarketPlace_NoProceeds();
         }
-        nft.approve(address(0), tokenId);
-        emit CanceledItem(nftAddress, tokenId);
+        proceeds = 0;
+        (bool success, ) = payable(msg.sender).call{value: proceeds}("");
+        if (!success) {
+            revert NftMarketPlace__TransferFailed();
+        }
     }
 
     // check if NFT is not already listed
